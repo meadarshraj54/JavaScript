723e1ac67f140bd1cc5f22ed27ee2eb28cbe1f8d
meadarshraj54 <165828409+meadarshraj54@users.noreply.github.com>
2025-10-16 19:01:01 +0000

    Variables and Datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; // treat all js code as newer version
+
+let name = "Adarsh" // String
+let age = 20 // integer
+let status = true // boolean
+
+// null data type - standalone value
+// undefined - not initialised value
+// symbol - unique
+
+// object
+
+console.log(typeof(null)) // object
+console.log(typeof(undefined))// undefined
---
+++ Basics/variables.js
@@ -0,0 +1,22 @@
+const id = 5 // Constatnts Cannot be changed
+let a = "Adarsh"
+var x = 15
+
+let y // by default undefine value
+
+// id = 2  : unable to update as const cant be changed
+a = "Rahul"
+x = "DBUU"
+
+/*
+We have to use onlu const and let 
+Avoid using var
+*/
+
+// console.log(id)
+// console.log(a)
+// console.log(x)
+
+console.log(y) // by default undefined
+
+console.table([id,x,a])
