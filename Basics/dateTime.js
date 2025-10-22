849fe70f54b86974f8aa7f8ed7eac6a56a42a8da
meadarshraj54 <165828409+meadarshraj54@users.noreply.github.com>
2025-10-22 17:42:46 +0000

    Date and Time

---
+++ Basics/dateTime.js
@@ -0,0 +1,14 @@
+let today = new Date();
+
+console.log(typeof today);
+
+console.log(today.toString());
+
+console.log(today.toDateString());
+
+console.log(today.toLocaleDateString());
+
+console.log(today.toLocaleString());
+
+console.log(today.toLocaleTimeString());
+
