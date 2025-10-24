aaa869dbb13acb59372d34bf43b452a3fb6cd68f
meadarshraj54 <165828409+meadarshraj54@users.noreply.github.com>
2025-10-24 18:00:16 +0000

    JSOn

---
+++ Objects/part2.js
@@ -0,0 +1,17 @@
+const obj = {
+    course : "JS",
+    courseInstructor : "Hitesh",
+    price : 999
+}
+
+const {courseInstructor : instructor } = obj  // ---> object de-structure
+
+console.log(instructor);
+
+
+// JSON
+
+{
+    "name" : "adarsh",
+    "email" : "abc@gmail.com"
+}
